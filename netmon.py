c7daea239cf9b0b042b501b53abeeabdf2fad0bc
shahar <lshahar9@gmail.com>
2023-04-21 22:47:22 +0300

    fixed initial stuff

--- netmon.py
+++ netmon.py
@@ -2,9 +2,10 @@ import threading
 import time
 import nmap
 import argparse
-from scapy.all import ARP, Ether, srp
+from scapy.all import *
 from prettytable import PrettyTable
 from colorama import Fore, Style
+from scapy.layers.l2 import ARP, Ether
 
 # Define the network to scan
 network = "192.168.1.0/24"
@@ -24,10 +25,10 @@ def scan_network():
     # Create an ARP request packet
     arp = ARP(pdst=network)
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-    packet = ether / arp
+    Packet1 = ether / arp
 
     # Send the packet and get the response
-    result = srp(packet, timeout=args.timeout, verbose=0)[0]
+    result = srp(Packet1, timeout=args.timeout, verbose=0)[0]
 
     # Update the devices dictionary with the response
     for sent, received in result:

